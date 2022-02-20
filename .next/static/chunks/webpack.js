8cecd72a72eeb9dcc34dae9cf9593f538f13be6d
Harshwardhan <harshafartale12@gmail.com>
2022-02-20 15:57:06 +0530

    polygon integration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12b1b04cfd9f09a5"; }
+/******/ 		__webpack_require__.h = function() { return "4542e8225ff93ced"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,27 @@
-const path = require("path");
-
+const path = require('path')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+const mnemonic = fs.readFileSync('.secret').toString().trim()
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     development: {
-      network_id: "*",
-      host: "localhost",
+      network_id: '*',
+      host: 'localhost',
       // port: 7545, // for ganache gui
       port: 8545, // for ganache-cli
       gas: 6721975,
       gasPrice: 20000000000,
     },
+    matic: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
   },
-};
+}
